7b5c1b156e98ae0a518e7a19ee5f13357327a70d
Sam Black <samblack@gmail.com>
2024-08-22 11:27:16 -0500

    Initial commit - Day 1 notes

--- src/Primary.java
+++ src/Primary.java
@@ -1,5 +1,17 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+public class Primary {
+    public static void main(String[] args) {    //start of main method
+        System.out.print("G'mornin, All Saints!");  //this line does something
+        System.out.println(" How is everyone this morning?");
+
+        System.out.println("  \"It is hot \ttoday!\\");
+
+        System.out.println( 7 + 8 + 9 );
+        System.out.println( 7 + " " + 8 + 9 );
+        System.out.println( 7 + 8 + " " + 9 );
+        System.out.println( 7 + " " + (8 + 9) );
+
+        // single line comment
+
+    }   // end of main method
 }
+
