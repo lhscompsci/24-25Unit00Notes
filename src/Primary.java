b56c72ed7b72265e01f4e281734e6a444033b984
Sam Black <samblack@gmail.com>
2024-08-27 12:03:26 -0500

    Added notes about the different date types

--- src/Primary.java
+++ src/Primary.java
@@ -12,6 +12,35 @@ public class Primary {
 
 
 
+        // Variables
+        // 8 primitive types
+            // 4 integer types:  byte [-128,127], short [-32768,32767], int [-2147483648,2147483647], long
+        int bob = 2000000000;
+        System.out.println( 2 * bob );
+
+        System.out.println(Byte.MAX_VALUE);
+
+        System.out.println(Byte.MIN_VALUE);
+
+            // 2 real types: float 32-bit, double 64-bit
+        double fl = 13.2 + 1.2;
+        System.out.println((int)fl);
+            // 1 unsigned "integer" type:  char
+                // 'A' -> 65   1000001
+                // ' ' -> 32   0100000
+                // 'a' -> 97
+                // '0' -> 48
+        char let = 'q';
+        char l = 65;
+        char newline = '\n';
+
+        bob = let + l;
+        System.out.println( (char) bob );
+
+            // 1 boolean: true, false
+
+
+
         // single line comment
 
     }   // end of main method
